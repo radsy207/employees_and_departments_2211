eacb5b554274662f9f1abc73c7e98c8cb0d85808
David Marino <davidjmarino8@gmail.com>
2023-01-13 11:42:04 -0500

    Add: list departments

--- lib/budget.rb
+++ lib/budget.rb
@@ -1,7 +1,18 @@
 class Budget
-  attr_reader :year
+  attr_reader :year, :departments
 
   def initialize(year)
     @year = year
+    @departments = []
+  end
+
+  def add_department(department)
+    @departments << department
+  end
+
+  def list_departments
+    @departments.map do |department|
+      department
+    end
   end
 end
--- spec/budget_spec.rb
+++ spec/budget_spec.rb
@@ -4,14 +4,37 @@ require './lib/budget'
 
 RSpec.describe Budget do
   let(:customer_service) {Department.new("Customer Service")}
+  let(:sales) {Department.new("Sales")}
+  let(:fire) {Department.new("Fire")}
   let(:bobbi) {Employee.new({name: "Bobbi Jaeger", age: "30", salary: "$100000"})}
   let(:aaron) {Employee.new({name: "Aaron Tanaka", age: "25", salary: "90000"})}
   let(:budget1) {Budget.new("2023")}
 
   describe '#initialize' do
     it 'exists' do
-      
       expect(budget1).to be_a(Budget)
+    end
+
+    it 'has readable attributes' do
+      expect(budget1.year).to eq("2023")
+      expect(budget1.departments).to eq([])
+    end
+  end
+
+  describe "#add_department" do
+    it 'adds a department to its departments array' do
+      budget1.add_department(customer_service)
+      budget1.add_department(sales)
+      expect(budget1.departments).to match([customer_service, sales])
+    end
+  end
+
+  describe '#list_departments' do
+    it 'lists departments that a budget has' do
+      budget1.add_department(customer_service)
+      budget1.add_department(sales)
+      budget1.add_department(fire)
+      expect(budget1.list_departments).to match([customer_service, sales, fire])
     end
   end
 end
