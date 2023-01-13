ff58e4d3d000648ae9cf6fce72441546c167482f
David Marino <davidjmarino8@gmail.com>
2023-01-13 11:07:44 -0500

    Initial commit

---
+++ spec/employee_spec.rb
@@ -0,0 +1,20 @@
+require './lib/employee'
+
+RSpec.describe Employee do
+  let(:bobbi) {Employee.new({name: "Bobbi Jaeger", age: "30", salary: "$100000"})}
+
+
+
+  describe '#initialize' do
+    it 'exists' do
+      
+      expect(bobbi).to be_a(Employee)
+    end
+
+    it 'has readable attributes' do
+      expect(bobbi.name).to eq("Bobbi Jaeger")
+      expect(bobbi.age).to eq(30)
+      expect(bobbi.salary).to eq(100000)
+    end
+  end
+end
